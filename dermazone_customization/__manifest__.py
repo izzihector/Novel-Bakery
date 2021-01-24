128531ac41a66ee53fa8953ff76300c6ef850d98
umair0099 <umairsabir674@gmail,com>
2021-01-24 12:16:05 +0300

    Update all customization

--- dermazone_customization/__manifest__.py
+++ dermazone_customization/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Dermazone Customizations',
+    'name': 'Dermazone Customizations.',
     'version': '1.1',
     'summary': 'Customized module for all the general requirements of dermazone project',
     'sequence': 3,
@@ -12,7 +12,8 @@
     ],
     'data': [
         'views/views.xml',
-        'views/reports.xml'
+        'views/reports.xml',
+        'reports/evaluation_report.xml'
     ],
     'installable': True,
     'application': True,
--- dermazone_customization/models/model.py
+++ dermazone_customization/models/model.py
@@ -1,10 +1,52 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 class InheritSaleOrderLines(models.Model):
     _inherit = "sale.order.line"
 
-    lot_number = fields.Many2one('stock.production.lot')
+    lot_number = fields.Many2one('stock.production.lot', change_default=True, ondelete='restrict')
     expiry_date = fields.Datetime(related='lot_number.use_date')
+    bonus_qty = fields.Float()
+
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        """
+        Prepare the dict of values to create the new invoice line for a sales order line.
+
+        :param qty: float quantity to invoice
+        """
+        self.ensure_one()
+        res = {}
+        product = self.product_id.with_context(force_company=self.company_id.id)
+        account = product.property_account_income_id or product.categ_id.property_account_income_categ_id
+
+        if not account and self.product_id:
+            raise UserError(_('Please define income account for this product: "%s" (id:%d) - or for its category: "%s".') %
+                (self.product_id.name, self.product_id.id, self.product_id.categ_id.name))
+
+        fpos = self.order_id.fiscal_position_id or self.order_id.partner_id.property_account_position_id
+        if fpos and account:
+            account = fpos.map_account(account)
+
+        res = {
+            'name': self.name,
+            'sequence': self.sequence,
+            'origin': self.order_id.name,
+            'account_id': account.id,
+            'price_unit': self.price_unit,
+            'lot_number': self.lot_number.id,
+            'expire_date': self.expiry_date,
+            'quantity': qty,
+            'discount': self.discount,
+            'bonus_qty': self.bonus_qty,
+            'uom_id': self.product_uom.id,
+            'product_id': self.product_id.id or False,
+            'invoice_line_tax_ids': [(6, 0, self.tax_id.ids)],
+            'account_analytic_id': self.order_id.analytic_account_id.id,
+            'analytic_tag_ids': [(6, 0, self.analytic_tag_ids.ids)],
+            'display_type': self.display_type,
+        }
+        return res
 
     @api.onchange('product_id')
     def change_lot_number(self):
@@ -19,8 +61,9 @@ class InheritSaleOrderLines(models.Model):
 class InheritInvoiceLines(models.Model):
     _inherit = 'account.invoice.line'
 
-    lot_number = fields.Many2one('stock.production.lot')
+    lot_number = fields.Many2one('stock.production.lot', change_default=True, ondelete='restrict')
     expire_date = fields.Datetime(related='lot_number.use_date')
+    bonus_qty = fields.Float()
 
     @api.onchange('product_id')
     def change_lot_number(self):
@@ -32,4 +75,8 @@ class InheritInvoiceLines(models.Model):
         return {'domain': {'lot_number': [('id', 'in',lot_list)]}}
 
 
+class InheritMoveLines(models.Model):
+    _inherit = 'stock.move.line'
 
+    lot_id = fields.Many2one('stock.production.lot', 'Lot/Serial Number')
+    product_stock = fields.Float(related='lot_id.product_qty')
